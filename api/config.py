00a28d76d8880e9388fa5867620d999592c7d4c0
VictorMS05 <135737262+VictorMS05@users.noreply.github.com>
2024-05-14 22:42:34 -0600

    modifique llamada de variables de entorno

--- api/config.py
+++ api/config.py
@@ -1,15 +1,17 @@
 """Módulo de la configuración de la aplicación"""
 
+import os
 from dotenv import load_dotenv
 
 class DevelopmentConfig():
     """Clase de configuración para desarrollo"""
+    load_dotenv()
     DEBUG = True
-    MYSQL_HOST = load_dotenv().get('MYSQL_HOST')
-    MYSQL_USER = load_dotenv().get('MYSQL_USER')
-    MYSQL_PASSWORD = load_dotenv().get('MYSQL_PASSWORD')
-    MYSQL_DB = load_dotenv().get('MYSQL_DB')
-    MYSQL_PORT = 3306
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
+    MYSQL_PORT = os.getenv('MYSQL_PORT')
 
 diccionario_de_configuraciones = { # Diccionario de configuraciones
     'development': DevelopmentConfig # Configuración de desarrollo
